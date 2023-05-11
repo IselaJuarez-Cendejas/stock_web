19e74d460bff70047195a3218fb97800aadf74e3
Ubuntu <juarezij@miamioh.edu>
2023-05-11 04:14:06 +0000

    Updating atock.js and html files

--- final.php
+++ final.php
@@ -1,26 +1,62 @@
-<?php 
+<?php
 /*
-   scott campbell
-   Kurt Johnson
-   display temps
- */
-$DBSTRING = "sqlite:cse383.db";  // This must be updated to your real database location 
-include "sql.inc";
-include "final.class.php";
+scott campbell
+Kurt Johnson
+display temps
+*/
 
-header ("Access-Control-Allow-Origin: *");
-header ("Access-Control-Allow-Methods: GET,POST,PUSH,OPTIONS");
-header ("content-type: application/json");
-header ("Access-Control-Allow-Headers: Content-Type");
-require_once "RestServer.php";
+$DBSTRING = "sqlite:cse383.db"; // This must be updated to your real database location 
+include "sql.inc";
+
 // phpinfo();
 
-// Kurt Johnson
-// with linux function code by Scott Campbell
+// This function is for a POST request that will save data into the database
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $ticker = $_POST['ticker'];
+  $jsonData = $_POST['jsonData'];
+  $date = $_POST['date'];
+  $queryType = $_POST['queryType'];
+  $sql = "INSERT INTO stock (stockTicker, jsonData, dateTime, queryType) VALUES ('$ticker', '$jsonData', '$date', '$queryType')";
+  $db = new PDO($DBSTRING);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $db->exec($sql);
+} else if ($_SERVER["REQUEST_METHOD"] == 'GET') {
+  $startdate;
+  $enddate;
+  $limit;
+  $ticker;
 
+  if (isset($_GET['startdate'])) {
+    $startdate = $_GET['startdate'];
+  } else {
+    $startdate = '2022-01-01';
+  }
 
-$method=$_REQUEST["method"];
-// example request: http://path/to/resource/rest/api/vi/sayHello?&name=World
+  if (isset($_GET['enddate'])) {
+    $enddate = $_GET['enddate'];
+  } else {
+    $enddate = '2025-01-01';
+  }
 
-$rest = new RestServer (new final_rest(),$method);
-$rest->handle ();
+  if (isset($_GET['limit'])) {
+    $limit = $_GET['limit'];
+  } else {
+    $limit = 100;
+  }
+
+  if (isset($_GET['ticker'])) {
+    $ticker = $_GET['ticker'];
+  } else {
+    $ticker = "%";
+  }
+
+  $sql = "SELECT * FROM stock WHERE dateTime BETWEEN ? AND ? AND stockTicker LIKE ? LIMIT ?";
+
+  try {
+    $data = GET_SQL($sql, $startdate, $enddate, $ticker, $limit);
+    echo json_encode($data);
+  } catch (Exception $e) {
+    echo $e->getMessage();
+  }
+}
+?>
--- stock.js
+++ stock.js
@@ -2,80 +2,82 @@ var API_KEY = 'EyXYU6QqaPsfWfGxGp4uPo0Kj0RGur3E';
 var URL = 'https://api.polygon.io';
 
 const EXCHANGES = [
-  { id: 'NYSE', name: 'New York Stock Exchange', mic: 'XNYS' },
-  { id: 'NASDAQ', name: 'Nasdaq Stock Market', mic: 'XNAS' },
-  { id: 'AMEX', name: 'NYSE American', mic: 'XASE' },
-  { id: 'ARCA', name: 'NYSE Arca', mic: 'ARCX' },
-  { id: 'BATS', name: 'Cboe BZX Exchange', mic: 'BATS' }
+	{ id: 'NYSE', name: 'New York Stock Exchange', mic: 'XNYS' },
+	{ id: 'NASDAQ', name: 'Nasdaq Stock Market', mic: 'XNAS' },
+	{ id: 'AMEX', name: 'NYSE American', mic: 'XASE' },
+	{ id: 'ARCA', name: 'NYSE Arca', mic: 'ARCX' },
+	{ id: 'BATS', name: 'Cboe BZX Exchange', mic: 'BATS' }
 ];
 
-$(document).ready(function() {
+$(document).ready(function () {
 	getExchanges();
 	$('#exchangeSelect').on('change', getStocks);
 	// Add event listener to detailsButton
-	$('#detailsButton').on('click', function() {
+	$('#detailsButton').on('click', function () {
 		// Toggle visibility of details table
 		$('.details-table').toggle();
 		getDetails();
 	});
+	$('#newsButton').on('click', function () {
+		// Toggle visibility of details table
+		//$('.news-table').toggle();
+		getNews();
+	});
 });
 
 function getExchanges() {
-  // Clear the existing options in the exchange dropdown
-  $('#exchangeSelect').html('');
+	$('#exchangeSelect').html('');
+	$('<option>').val('').text('--Select an Exchange--').appendTo('#exchangeSelect');
 
-  // Add a default option
-  $('<option>').val('').text('--Select an Exchange--').appendTo('#exchangeSelect');
-
-  // Add pre-filled options for hard-coded exchanges
-  $.each(EXCHANGES, function(i, exchange) {
-    $('<option>').val(exchange.id).text(exchange.name + ' (' + exchange.mic + ')').appendTo('#exchangeSelect');
-  });
+	// Add pre-filled options for hard-coded exchanges
+	$.each(EXCHANGES, function (i, exchange) {
+		$('<option>').val(exchange.id).text(exchange.name + ' (' + exchange.mic + ')').appendTo('#exchangeSelect');
+	});
 }
 
 function getStocks() {
 	var exchange = $('#exchangeSelect').val();
 	$('#stockSelect').html('');
 	$('<option>').val('').text('--Select a Stock--').appendTo('#stockSelect');
-  
+
 	// Get all stocks from the selected exchange
-	a=$.ajax({
-	  url: URL + '/v3/reference/tickers',
-	  method: 'GET',
-	  data: {
-		apiKey: API_KEY,
-		primary_exchange: exchange,
-		active: true
-	  }
-	}).done(function(data) {
-	  //console.log(data);
-	  var stocks = data.results;
-	  $.each(stocks, function(i, stock) {
-		$('<option>').val(stock.ticker).text(stock.name).appendTo('#stockSelect');
-	  });
-	}).fail(function(error) {
-	  console.log('error', error.statusText);
+	a = $.ajax({
+		url: URL + '/v3/reference/tickers',
+		method: 'GET',
+		data: {
+			apiKey: API_KEY,
+			primary_exchange: exchange,
+			active: true
+		}
+	}).done(function (data) {
+		//console.log(data);
+		var stocks = data.results;
+		$.each(stocks, function (i, stock) {
+			$('<option>').val(stock.ticker).text(stock.name).appendTo('#stockSelect');
+		});
+	}).fail(function (error) {
+		console.log('error', error.statusText);
 	});
 }
 
-function getDetails() {
+async function getDetails() {
 	var stock = $('#stockSelect').val();
+	var returndata = [];
 	var now = new Date().getTime();
 	var from = now - 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
 	var stockDetailsUrl = URL + '/v2/aggs/ticker/' + stock + '/range/1/day/' + from + '/' + now;
-	a=$.ajax({
+	a = $.ajax({
 		url: stockDetailsUrl,
 		method: 'GET',
 		data: {
 			apiKey: API_KEY,
 		}
-	}).done(function(data) {
+	}).done(function (data) {
 		console.log(data);
 		// Display data on stocks.html
-		// Populate the details table with the retrieved data
 		var detailsTable = $('.details-table');
 		detailsTable.empty();
-		$.each(data.results, function(i, result) {
+		$.each(data.results, function (i, result) {
 			var detailsRow = $('<tr>');
 			$('<td>').text(new Date(result.t).toLocaleString()).appendTo(detailsRow);
 			$('<td>').text(result.o).appendTo(detailsRow);
@@ -85,8 +87,72 @@ function getDetails() {
 			$('<td>').text(result.v).appendTo(detailsRow);
 			detailsRow.appendTo(detailsTable);
 		});
-	}).fail(function(error) {
+		console.log(data.results);
+		tickerDetails(data.results);
+	}).fail(function (error) {
+		console.log('error', error.statusText);
+	});
+	return returndata;
+}
+
+async function tickerDetails(prices) {
+	var stock = $('#stockSelect').val();
+	var stockDetailsUrl = URL + "/v3/reference/tickers/" + stock;
+	a = $.ajax({
+		url: stockDetailsUrl,
+		method: 'GET',
+		data: {
+			apiKey: API_KEY,
+		}
+	}).done(function (data) {
+		console.log(data);
+		// Display data on stocks.html
+		$("#tickerName").text(data.results.name);
+		$("#tickerDescription").text(data.results.description);
+		$("#tickerLogo").attr("src", data.results.branding.logo_url + "?apiKey=" + API_KEY);
+		saveToDatabase({
+			"ticker": data.results.ticker,
+			"tickerDetails": data.results,
+			"prices": prices
+		})
+	}).fail(function (error) {
 		console.log('error', error.statusText);
 	});
 }
-  
+
+function saveToDatabase(json) {
+	$.ajax({
+		url: 'final.php',
+		method: 'POST',
+		data: {
+			queryType: 'tickerDetails',
+			jsonData: JSON.stringify(json),
+			date: new Date().toISOString().slice(0, 10),
+			ticker: $('#stockSelect').val()
+		}
+	});
+}
+
+async function getNews() {
+	var stock = $('#stockSelect').val();
+	var stockNewssUrl = URL + "/v2/reference/news";
+	a = $.ajax({
+		url: stockNewssUrl,
+		method: 'GET',
+		data: {
+			ticker: stock,
+			apiKey: API_KEY
+		}
+	}).done(function (data) {
+		console.log(data);
+		// Display data on stocks.html
+		$.each(data.results, function (i, result) {
+			$('#newsTitle').text(result.title);
+			$('newsDate').text(result.timestamp);
+			$('#newsSource').text(result.source);
+			$('newsDescription').text(result.description);
+		});
+	}).fail(function (error) {
+		console.log('error', error.statusText);
+	});
+} 
